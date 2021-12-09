b7d88e49f7002d2bf7b63a2b0bba57fb51329d8e
Lizmadelyne <lizfajardop@gmail.com>
2021-12-09 15:02:46 -0500

    creando el login

---
+++ src/app/modulos/administracion/administracion.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdministracionRoutingModule } from './administracion-routing.module';
+import { CrearPersonaComponent } from './personaas/crear-persona/crear-persona.component';
+import { EditarPersonaComponent } from './personas/editar-persona/editar-persona.component';
+import { EliminarPersonaComponent } from './personas/eliminar-persona/eliminar-persona.component';
+import { BuscarPersonaComponent } from './personas/buscar-persona/buscar-persona.component';
+import { CrearProductoComponent } from './productos/crear-producto/crear-producto.component';
+import { EditarProductosComponent } from './productos/editar-productos/editar-productos.component';
+import { EliminarProductoComponent } from './productos/eliminar-producto/eliminar-producto.component';
+import { BuscarProductoComponent } from './productos/buscar-producto/buscar-producto.component';
+
+
+@NgModule({
+  declarations: [
+    CrearPersonaComponent,
+    EditarPersonaComponent,
+    EliminarPersonaComponent,
+    BuscarPersonaComponent,
+    CrearProductoComponent,
+    EditarProductosComponent,
+    EliminarProductoComponent,
+    BuscarProductoComponent
+  ],
+  imports: [
+    CommonModule,
+    AdministracionRoutingModule
+  ]
+})
+export class AdministracionModule { }
